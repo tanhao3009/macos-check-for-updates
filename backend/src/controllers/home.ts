8f9a867634896cec6e27f61ea680dffdf68471c4
Hung Q. Truong <htq287@gmail.com>
2019-08-18 20:19:06 +0700

    * remove un-used code

--- backend/src/controllers/home.ts
+++ backend/src/controllers/home.ts
@@ -3,14 +3,4 @@ export let getUsages = (req: Request, res: Response) => {
   res.render("home", {
     title: "Home"
   });
-  // res.send(`
-  //   <h1>Usages</h1>
-  //   <p>Please refer to <a href="https://github.com/hungtq287/macos-check-for-updates/tree/master/Backend" target="blank">my tutorial</a> for details.</p>
-  //   <ul>
-  //     <li>GET /files   - list all upload files</li>
-  //     <li>GET /files/{id} - get one uploaded file</li>
-  //     <li>POST /upload/file - handle single file upload</li>
-  //     <li>POST /upload/files - handle multiple files upload</li>
-  //   </ul>
-  // `);
 };
